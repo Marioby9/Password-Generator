e0d5a5af3c73b89cef7966ee27004ef89320e9aa
Mario Martín <95193489+Marioby9@users.noreply.github.com>
2023-04-13 23:18:58 +0200

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Password_Generator {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires java.datatransfer;
-	requires java.desktop;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
